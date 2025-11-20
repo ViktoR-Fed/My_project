b6bd59f0fdf34e1ed9446897ffd0944558205711
Виктор <20viktor120@gmail.com>
2025-11-20 19:01:18 +0300

    add func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,18 @@
-from typing import Iterable
+from typing import Any, Dict, List
 
-def filter_by_state(data:[Iterable[list]], state: str = "EXECUTED") -> Iterable[list]:
-    ''' Функция сортировки списка словарей по значению 'state' '''
+
+def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+    """Функция сортировки списка словарей по значению 'state'"""
     new_list = []
     for item in data:
-        if item['state'] == state:
+        if item["state"] == state:
             new_list.append(item)
 
     return new_list
 
 
-
-
+def sort_by_date(list_dict: List[Dict[str, Any]], ascending: bool = True) -> List[Dict[str, Any]]:
+    """Функция для сортировки списка словарей по параметру 'key_sort',
+    порядок сортировки 'decreasing'(убывающий)"""
+    sorted_list = sorted(list_dict, key=lambda x: x["date"], reverse=ascending)
+    return sorted_list
