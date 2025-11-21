8ffda42808cd4eec3a51b46fd8d6cbf25275b77f
Виктор <20viktor120@gmail.com>
2025-11-21 17:18:20 +0300

    add readme.md file

--- src/processing.py
+++ src/processing.py
@@ -12,7 +12,7 @@ def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List
 
 
 def sort_by_date(list_dict: List[Dict[str, Any]], ascending: bool = True) -> List[Dict[str, Any]]:
-    """Функция для сортировки списка словарей по параметру 'key_sort',
-    порядок сортировки 'decreasing'(убывающий)"""
+    """Функция для сортировки списка словарей,
+    порядок сортировки 'ascending'(убывающий)"""
     sorted_list = sorted(list_dict, key=lambda x: x["date"], reverse=ascending)
     return sorted_list
